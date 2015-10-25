03d5ca7055759fe9952d4c01965a937a2d2a02a1
James Cool <jeffreysbrother@gmail.com>
2015-10-25 16:27:00 -0700

    styled blog pages

--- wp-content/themes/smear/home.php
+++ wp-content/themes/smear/home.php
@@ -2,11 +2,11 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-md-10 col-md-offset-1 last-paragraph">
+    <div class="col-md-8 col-md-offset-2 last-paragraph">
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h1><a class="blog-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
         <p><?php the_category(); ?></p>
         <p class="text-muted"><?php the_date(); ?></p>
--- wp-content/themes/smear/single.php
+++ wp-content/themes/smear/single.php
@@ -1,16 +1,20 @@
 <?php get_header(); ?>
 
-<div class="container last-paragraph">
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2 last-paragraph">
 
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <h1><a  class="blog-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <p class="text-muted"><?php the_content(); ?></p>
 
   <?php endwhile; else : ?>
     <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
   <?php endif; ?>
 
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
