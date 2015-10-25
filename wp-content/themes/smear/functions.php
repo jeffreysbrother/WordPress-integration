db0b9cc54e54b7795c8e231643afcbda7d427157
James Cool <jeffreysbrother@gmail.com>
2015-10-25 02:57:35 -0700

    fixed a few errors in functions dot php file

--- wp-content/themes/smear/functions.php
+++ wp-content/themes/smear/functions.php
@@ -6,7 +6,7 @@
     wp_enqueue_style( 'blueimp-css', '//blueimp.github.io/Gallery/css/blueimp-gallery.min.css' );
     wp_enqueue_style( 'gallery', get_template_directory_uri() . '/css/bootstrap-image-gallery.min.css' );
     wp_enqueue_style( 'font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
-    wp_enqueue_style( 'badges', get_template_directory_uri() . '/badges.css' );
+    wp_enqueue_style( 'badges', get_template_directory_uri() . '/css/badges.css' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_styles' );
@@ -17,8 +17,8 @@
     wp_enqueue_script( 'blueimp-js', '//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'image-gallery-js', get_template_directory_uri() . '/js/bootstrap-image-gallery.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
-    wp_enqueue_script( 'reportcard-js', get_template_directory_uri() . '/js/reportcard.min.js', array('jquery'), '', true );
-    wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true );
+    wp_enqueue_script( 'reportcard-js', get_template_directory_uri() . '/js/reportcard.min.js', array('jquery', '/js/bootstrap.min.js'), '', true );
+    wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', array('jquery', '/js/reportcard.min.js'), '', true );
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_js' );
 
