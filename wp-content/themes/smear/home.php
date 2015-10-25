1957a5b6909bdd339b94f52db6fd2a4ea5d6a1d7
James Cool <jeffreysbrother@gmail.com>
2015-10-25 13:46:48 -0700

    tried adding text-muted class with no luck

--- wp-content/themes/smear/home.php
+++ wp-content/themes/smear/home.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
 
-<div class="container last-paragraph">
+<div class="container">
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1 last-paragraph">
 
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
-    <p><?php the_category(); ?></p>
-    <p><?php the_date(); ?></p>
-    <p><?php the_excerpt(); ?></p>
+        <p><?php the_category(); ?></p>
+        <p class="text-muted"><?php the_date(); ?></p>
+        <p class="text-muted"><?php the_excerpt(); ?></p>
 
-  <?php endwhile; else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-  <?php endif; ?>
+      <?php endwhile; else : ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+      <?php endif; ?>
 
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/smear/single.php
+++ wp-content/themes/smear/single.php
@@ -5,7 +5,7 @@
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <p><?php the_content(); ?></p>
+    <p class="text-muted"><?php the_content(); ?></p>
 
   <?php endwhile; else : ?>
     <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
