12f5975b3637eb65aed3dbbe091bfe6650f76690
James Cool <jeffreysbrother@gmail.com>
2015-11-05 11:25:08 -0800

    removed modals associated with track titles, left aesthetics button modal

--- wp-content/themes/smear/js/app.js
+++ wp-content/themes/smear/js/app.js
@@ -17,17 +17,4 @@
         });
 
 
-        //this controls the bootstrap modal on the music page
-        $('#exampleModal').on('show.bs.modal', function (event) {
-          var button = $(event.relatedTarget); // Button that triggered the modal
-          var recipient = button.data('whatever');
-          var body = button.data('information');
-          // Extract info from data-* attributes
-          // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-          // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-          var modal = $(this);
-          modal.find('.modal-title').text(recipient);
-          modal.find('.modal-body').text(body);
-        });
-
   });
