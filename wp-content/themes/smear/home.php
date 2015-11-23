7644db0439dabaa46faf167e8385fa06e1aebcca
James Cool <jeffreysbrother@gmail.com>
2015-11-23 00:04:33 -0800

    updated home and single pages to fix missing BY before author name

--- wp-content/themes/smear/home.php
+++ wp-content/themes/smear/home.php
@@ -8,7 +8,7 @@
 
         <h2><a class="blog-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-        <p class="text-muted"><?php the_time('F jS, Y'); ?> | <?php the_author(); ?> <span class="pipe">|</span> <span class="category-part">Posted in
+        <p class="text-muted"><?php the_time('F jS, Y'); ?> | by <?php the_author(); ?> <span class="pipe">|</span> <span class="category-part">Posted in
 
             <?php
               $categories = get_the_category();
--- wp-content/themes/smear/single.php
+++ wp-content/themes/smear/single.php
@@ -6,7 +6,7 @@
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h2><a  class="blog-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h2><a class="blog-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
         <p class="text-muted"><?php the_time('F jS, Y'); ?> | by <?php the_author(); ?> <span class="pipe">|</span> <span class="category-part">Posted in
 
